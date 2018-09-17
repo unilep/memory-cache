0c1a7f1e55dd2f5df2dfca41098ed6a3c865d4cb
kimjeonggi <sirius@lotte.net>
2018-09-17 16:28:05 +0900

    del

--- cache/CacheContainer.java
+++
@@ -1,7 +0,0 @@
-package cache;
-
-public interface CacheContainer<V> {
-	
-	long find(V value);
-	
-}
--- cache/algorithm/CacheAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package cache.algorithm;
-
-public interface CacheAlgorithm<V> {
-
-	void whenMiss(V obj);
-	
-	void whenHit(V obj);
-	
-}
